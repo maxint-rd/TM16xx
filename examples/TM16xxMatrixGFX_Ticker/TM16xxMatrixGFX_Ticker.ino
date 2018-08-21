874b85e696e5629bf5aa7ca2f2e8a446b9efd94c
Maxint R&D <mm_rd@maxint.nl>
2018-08-21 11:13:16 +0200

    cleanup

--- examples/TM16xxMatrixGFX_Ticker/TM16xxMatrixGFX_Ticker.ino
+++ examples/TM16xxMatrixGFX_Ticker/TM16xxMatrixGFX_Ticker.ino
@@ -6,25 +6,20 @@
  * Based on Ticker.pde example of the MAX72xxPanel library
  * 
  * Confirmed to work in the following environments:
- *     - WeMos D1-mini and TM1640 8x8 MatrixLED Shield using Arduino IDE 1.6.10: DIN=D7/13/MOSI, CLK=D5/14/SCK, 253.244 bytes flash, 32.388 bytes RAM
+ *     - WeMos D1-mini and TM1640 8x8 MatrixLED Shield using Arduino IDE 1.6.10: DIN=D7/13/MOSI, CLK=D5/14/SCK, 253.516 bytes flash, 32.372 bytes RAM
  * 
+ * For more information see  https://github.com/maxint-rd/TM16xx
  */
 
 #include <Adafruit_GFX.h>
 #include <TM1640.h>
 #include <TM16xxMatrixGFX.h>
 
-//int pinCS = 10; // Attach CS to this pin, DIN to MOSI and CLK to SCK (cf http://arduino.cc/en/Reference/SPI )
-int numberOfHorizontalDisplays = 1;
-int numberOfVerticalDisplays = 1;
-
-//Max72xxPanel matrix = Max72xxPanel(pinCS, numberOfHorizontalDisplays, numberOfVerticalDisplays);
 //TM1640 module(9, 10);    // DIN=9, CLK=10
 TM1640 module(13, 14);    // For ESP8266/WeMos D1-mini: DIN=D7/13/MOSI, CLK=D5/14/SCK
-#define MATRIX_NUMCOLUMNS 16    // number of GRD lines, will be the y-height of the display
-#define MATRIX_NUMROWS 8    // number of SEG lines, will be the x-width of the display
-TM16xxMatrixGFX matrix(&module, MATRIX_NUMCOLUMNS, MATRIX_NUMROWS);    // TM16xx object, columns, rows
-
+#define MODULE_SIZECOLUMNS 16    // number of GRD lines, will be the y-height of the display
+#define MODULE_SIZEROWS 8    // number of SEG lines, will be the x-width of the display
+TM16xxMatrixGFX matrix(&module, MODULE_SIZECOLUMNS, MODULE_SIZEROWS);    // TM16xx object, columns, rows
 
 String tape = "Arduino";
 int wait = 100; // In milliseconds
@@ -35,22 +30,12 @@ int width = 5 + spacer; // The font width is 5 pixels
 void setup()
 {
   matrix.setIntensity(1); // Use a value between 0 and 7 for brightness
-  matrix.setRotation(3);
-  //matrix.setMirror(true);   // set X-mirror true when using the WeMOS D1 mini Matrix LED Shield
-
-// Adjust to your own needs
-//  matrix.setPosition(0, 0, 0); // The first display is at <0, 0>
-//  matrix.setPosition(1, 1, 0); // The second display is at <1, 0>
-//  matrix.setPosition(2, 2, 0); // The third display is at <2, 0>
-//  matrix.setPosition(3, 3, 0); // And the last display is at <3, 0>
-//  ...
-//  matrix.setRotation(0, 2);    // The first display is position upside down
-//  matrix.setRotation(3, 2);    // The same hold for the last display
+  matrix.setRotation(1);
+  matrix.setMirror(true);   // set X-mirror true when using the WeMOS D1 mini Matrix LED Shield (X0=Seg1/R1, Y0=GRD1/C1)
 }
 
 void loop()
 {
-
   for ( int i = 0 ; i < width * tape.length() + matrix.width() - 1 - spacer; i++ ) {
 
     matrix.fillScreen(LOW);
@@ -67,6 +52,8 @@ void loop()
       letter--;
       x -= width;
     }
+    matrix.drawPixel(0, 0, HIGH); // mark axis-origin for reference
+    matrix.drawPixel(0, 3, HIGH);
 
     matrix.write(); // Send bitmap to display
 
