f3db90891517c23211f4bb70f0cf9b43c0008726
Maxint R&D <mm_rd@maxint.nl>
2018-10-15 14:42:03 +0200

    Added isPressed(), attachRelease() and some cleanup

--- examples/TM16xxButtons_clicks/TM16xxButtons_clicks.ino
+++ examples/TM16xxButtons_clicks/TM16xxButtons_clicks.ino
@@ -4,7 +4,7 @@
   This example is for TM1638 LED&KEY module but can also be used for the extended TM1637 mini LED&KEY module.
   The extended TM1637 module has 4 push-buttons (KS1-KS4 to K2) and 4 bi-color leds (SEG1-SEG4 to GRID5/GRID6)
 
-  It was tested on Arduino Nano (old bootloader) in Arduino IDE 1.8.2. (7982B flash, 688B RAM)
+  It was tested on Arduino Nano (old bootloader) in Arduino IDE 1.8.2. (7436B flash, 668B RAM)
 
   Made by Maxint-RD MMOLE 2018. see GitHub.com/maxint-rd/TM16xx
 */
@@ -49,45 +49,72 @@ void setup()
   module.setupDisplay(true, 7);       // set intensity 0-7, 7=highest
   delay(50);
 
-  // show some text
+  // Show some text.
+  // Note: the TM16xxDisplay provides the println() function which is used in the callback functions below.
   Serial.println(F("txt"));
   module.setDisplayToString("HALO");
   delay(400);
   module.setDisplayToString("YOUTUBE ");
   delay(400);
+  module.clearDisplay();
 
-  // link the button callback functions.
+  // Attach the button callback functions that are defined below
+  buttons.attachRelease(fnRelease);
   buttons.attachClick(fnClick);
   buttons.attachDoubleClick(fnDoubleclick);
   buttons.attachLongPressStart(fnLongPressStart);
   buttons.attachLongPressStop(fnLongPressStop);
   buttons.attachDuringLongPress(fnLongPress);
 
-  Serial.println(F("clear"));
-  module.clearDisplay();
   Serial.println(F("Setup done"));
 }
 
 void loop()
 {
-  char text[17];
+  static unsigned long ulTime=millis();
   uint32_t dwButtons=buttons.tick();
-
-  //sprintf(text, "%lu", millis());
-  ltoa(millis(), text, 10);   // DECIMAL = base 10
 
   if(dwButtons)
     display.setDisplayToHexNumber(dwButtons, 0, false);
   else
-    module.setDisplayToString(text);
+  {
+    // For best doubleclick detection, the loop() needs to be as fast as possible. 
+    // So instead of calling delay(100), we check if 100ms has passed.
+    if(millis()-ulTime>100)
+    {
+      char text[17];
+      //sprintf(text, "%lu", millis());
+      ltoa(millis(), text, 10);   // DECIMAL = base 10
+      module.setDisplayToString(text);
+      ulTime=millis();
+    }
+  }
 
-  delay(100);
+  //delay(100); // to keep the loop fast don't use delay(100) 
 }
 
 
 //
 // Button callback functions
 //
+
+// The Release function will be called when a button was released.
+// It can be used for fast actions when no click or double click needs to be detected.
+void fnRelease(byte nButton)
+{
+  // using isPressed or is LongPressed a shift-key can be implemented
+  if(buttons.isLongPressed(0))
+    Serial.print(F("Button 0 still longpressed. "));
+  else if(buttons.isPressed(0))
+    Serial.print(F("Button 0 still pressed. "));
+
+  Serial.print(F("Button "));
+  Serial.print(nButton);
+  Serial.println(F(" release."));
+  display.println(F("rels  "));
+  Blink(10,1);
+} // release
+
 
 // This function will be called when a button was pressed 1 time (without a second press).
 void fnClick(byte nButton)
