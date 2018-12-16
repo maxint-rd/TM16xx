acbe7be10feb7c67b48e8bbd29cf0660dd1115ba
Maxint R&D <mm_rd@maxint.nl>
2018-12-16 15:21:52 +0100

    moved debug statements to separate example

--- examples/TM16xx_setSegments/TM16xx_setSegments.ino
+++ examples/TM16xx_setSegments/TM16xx_setSegments.ino
@@ -13,8 +13,8 @@
   For setting more than 8 segments the similar function setSegments16 can be used.
 
   It was tested on Arduino Nano (old bootloader) in Arduino IDE 1.8.2. (TM1638: 4944B flash, 292B RAM, TM1650: 5434B flash, 286B RAM)
-  The Blink function and Serial print functions were added to allow easy debugging using a logical analyser.
-
+  It was tested on ATTiny44A (ATtinyCore/LTO enabled) in Arduino IDE 1.8.2. (TM1640: 2738B flash, 107B RAM)
+  
   Made by Maxint-RD MMOLE 2018. see GitHub.com/maxint-rd/TM16xx
 */
 
@@ -22,11 +22,14 @@
 //TM1637 module(3, 2);    //  DIO=3, CLK=2
 //TM1637 module(5, 4);  // DIO=5, CLK=4
 
-#include <TM1638.h>
-TM1638 module(8, 9, 7);   // DIO=8, CLK=9, STB=7
+//#include <TM1638.h>
+//TM1638 module(8, 9, 7);   // DIO=8, CLK=9, STB=7
 
 //#include <TM1638QYF.h>
 //TM1638QYF module(8, 9, 7);   // DIO=8, CLK=9, STB=7
+
+#include <TM1640.h>
+TM1640 module(9, 10);  // DA=9, CLK=10
 
 //#include <TM1650.h>
 //TM1650 module(A4, A5);  // SDA=A4, SCL=A5
@@ -35,59 +38,26 @@ TM1638 module(8, 9, 7);   // DIO=8, CLK=9, STB=7
 //TM1668 module(8, 9, 10);   // DIO=8, CLK=9, STB=7
 //TM1668 module(3, 2, 1, 7, true, 7);    //  DIO=3, CLK=2, STB=1, byte numDigits = 7, boolean activateDisplay = true, byte intensity = 7, displaymode=TM1668_DISPMODE_7x10
 
-// function to show some indicative blinks
-void Blink(int nDelay=500, int nTimes=1)
-{
-  for(int i=0; i<nTimes; i++)
-  {
-    digitalWrite(LED_BUILTIN, HIGH);    // switch on (or active low LED off for ESP)
-    delay(nDelay);
-    digitalWrite(LED_BUILTIN, LOW);    // switch off (or active low LED on for ESP)
-    delay(nDelay);
-  }
-}
-
 void setup()
 {
-  Serial.begin(115200);
-  Serial.println(F("TM16xx_setSegments example"));
-  Serial.println(F("TM1638: DIO=8, CLK=9, STB=7"));
-
-  module.setupDisplay(true, 7);     // set display on at highest intensity
-
-  Serial.println(F("Clear"));
-  module.clearDisplay();
-
-  Serial.println(F("Display string"));
-  Blink(10);
   module.setDisplayToString("HALO");
   delay(1000);  
 
-  Blink(10);
   module.clearDisplay();
   module.setDisplayToString("HALO1234", 0xF0);
   delay(1000);  
-
-  Serial.println(F("Setup done..."));
 }
 
 void loop()
 {
-  Blink(10);
   module.clearDisplay();
-  Blink(10);
-
-  for(byte nPos=0; nPos<8; nPos++)
+  for(byte nPos=0; nPos<16; nPos++)     // 16 positions (columns) on TM1640, most others have less
   {
-    Serial.print(F("Loop pos: "));
-    Serial.println(nPos);
-    //for(uint16_t nVal=0; nVal<=0x3FF; nVal=nVal*2+1)
     for(int nVal=0; nVal<=0xFF; nVal=nVal*2+1)     // max 8 segments on TM1638QYF module.
     {
       module.setSegments(nVal, nPos);   // note: you can use setSegments16() when module class supports > 8 segments (TM1638, TM1668)
       delay(100);
     }
-    Blink(10);
   }
   delay(1000);
 }
