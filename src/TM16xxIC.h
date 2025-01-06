b35cb175fcd4ba2ccead53b5fcd93d76e7f754ad
Maxint R&D <mm_rd@maxint.nl>
2025-01-06 14:04:17 +0100

    Tested support for TM1623 using DIO as DI/DO shorted

--- src/TM16xxIC.h
+++ src/TM16xxIC.h
@@ -31,7 +31,7 @@ Made by Maxint R&D. See https://github.com/maxint-rd
 #define TM16XX_IFTYPE_2WACK 6		// 2 wire non-addressable interface with ACK like TM1637: DIO/CLK
 /*TODO*/ #define TM16XX_IFTYPE_0WIRE 0    // 0 wire no interface, only useful for debugging
 /*DONT*/ #define TM16XX_IFTYPE_1WSER 1		// 1 wire serial interface like TM1652: DIN, fixed bitrate 19200
-/*TODO*/ #define TM16XX_IFTYPE_4WSPI 9		// 4 wire SPI-like interface like TM1623: DIN/DOU/CLK/STB (TODO: test hardware SPI)
+/*TODO*/ #define TM16XX_IFTYPE_4WSPI 9		// 4 wire SPI-like interface like TM1629/TM1623/: DIN/DOU/CLK/STB. Datasheets mention DI shorted to DO becomes DIO => IFTYPE_3WSER
 /*TODO*/ #define TM16XX_IFTYPE_4WSER 4		// 4 wire serial interface like TM1681: DA/WR/RD/CS (TODO: test hardware SPI)
 /*TODO*/ #define TM16XX_IFTYPE_5WSER 5		// 3 wire like TM1649: DIO/SCLK/STB; separate DATA/CLK or BIN0-BIN3 for touch
 /*DONT*/ #define TM16XX_IFTYPE_2WACKB 7		// 2 wire non-addressable interface with ACK like TM1650: DAT/CLK, deviating commands/bitorder/etc
@@ -74,12 +74,14 @@ struct if_ctrl_tm16xx
 //
 #define IC_TM1616  if_ctrl_tm16xx {TM16XX_IFTYPE_3WSER, 1, 4, TM16XX_IC_SEGMUX_0 | TM16XX_IC_SEGBYTES_1SK, 0x007F, 0, 0x00}  // SxG: 7x4 CC
 #define IC_TM1618  if_ctrl_tm16xx {TM16XX_IFTYPE_3WSER, 5, 7, TM16XX_IC_SEGMUX_3 | TM16XX_IC_SEGBYTES_2, 0x381F, 3, 0x12}  // SxG:8x4-5x7 CC/CA, K:5x1
+#define IC_TM1623  if_ctrl_tm16xx {TM16XX_IFTYPE_3WSER, 5, 7, TM16XX_IC_SEGMUX_3 | TM16XX_IC_SEGBYTES_2, 0x3FFF, 5, 0x3F}  // SxG: 14x4-11x7 CC/CA, K:10x3, dataheet v2.4 p13 has DI connected to DO.
 #define IC_TM1624  if_ctrl_tm16xx {TM16XX_IFTYPE_3WSER, 5, 7, TM16XX_IC_SEGMUX_3 | TM16XX_IC_SEGBYTES_2, 0x3FFF, 0, 0x00}  // SxG: 14x4-11x7 CC/CA
 #define IC_TM1628  if_ctrl_tm16xx {TM16XX_IFTYPE_3WSER, 5, 7, TM16XX_IC_SEGMUX_3 | TM16XX_IC_SEGBYTES_2, 0x3BFF, 5, 0x1B}  // SxG: 13x4-10x7 CC/CA, K:10x2, 
 #define IC_TM1637  if_ctrl_tm16xx {TM16XX_IFTYPE_2WACK, 5, 6, TM16XX_IC_SEGMUX_0 | TM16XX_IC_SEGBYTES_1, 0x00FF, TM16XX_KD_PRESS_NI | 1, 0x1F}  // SxG: 8x6 CA, K:8x2 single pressed key number 0-6 + B3/B4 inverted 
 #define IC_TM1638  if_ctrl_tm16xx {TM16XX_IFTYPE_3WSER, 1, 8, TM16XX_IC_SEGMUX_0 | TM16XX_IC_SEGBYTES_2, 0x03FF, 4, 0x77}  // SxG:10x8 CC/CA, K:8x3
 #define IC_TM1640  if_ctrl_tm16xx {TM16XX_IFTYPE_2WSER, 1, 16,TM16XX_IC_SEGMUX_0 | TM16XX_IC_SEGBYTES_1, 0x00FF, 0, 0x00}  // SxG: 8x16 CC/CA
 #define IC_TM1668  if_ctrl_tm16xx {TM16XX_IFTYPE_3WSER, 5, 7, TM16XX_IC_SEGMUX_3 | TM16XX_IC_SEGBYTES_2, 0x3BFF, 5, 0x1B}  // SxG: 13x4-10x7 CC/CA, K:10x2; same as TM1628A in smaller package
+/* TODO for TM1623: Test separate DI/DO with hardware SPI,  TM16XX_IFTYPE_4WSPI */
 
 //
 // UNTESTED chips, only based on datasheet information. CHECK BEFORE USE.
@@ -88,13 +90,12 @@ struct if_ctrl_tm16xx
 #define IC_TM1617  if_ctrl_tm16xx {TM16XX_IFTYPE_3WSER, 5, 7, TM16XX_IC_SEGMUX_1 | TM16XX_IC_SEGBYTES_2, 0x0FF0, 5, 0x09}  // SxG:8x2-7x3 CC, K:6x1, chinese datasheet LCSC, address is 14 bytes from 00H to 0DH, only use: GRID1, GRID2, GRID7, SEG5-SEG12
 #define IC_TM1620  if_ctrl_tm16xx {TM16XX_IFTYPE_3WSER, 5, 6, TM16XX_IC_SEGMUX_2 | TM16XX_IC_SEGBYTES_2, 0x30FF, 0, 0x00}  // SxG: 10x4-8x6 CC
 #define IC_TM1620B if_ctrl_tm16xx {TM16XX_IFTYPE_3WSER, 5, 7, TM16XX_IC_SEGMUX_3 | TM16XX_IC_SEGBYTES_2, 0x383F, 3, 0x12}  // SxG: 9x4-6x7 CC/CA, K:6x1
-/*TODO*/ #define IC_TM1623  if_ctrl_tm16xx {TM16XX_IFTYPE_4WSPI, 5, 7, TM16XX_IC_SEGMUX_3 | TM16XX_IC_SEGBYTES_2, 0x3FFF, 5, 0x3F}  // SxG: 14x4-11x7 CC/CA, K:10x3
-/*TODO*/ #define IC_TM1623C  if_ctrl_tm16xx {TM16XX_IFTYPE_4WSPI, 5, 7, TM16XX_IC_SEGMUX_3 | TM16XX_IC_SEGBYTES_2, 0x3FFF, 5, 0x3F}  // SxG: 14x4-11x7 CC/CA, K:10x3, same as TM1623?
+#define IC_TM1623C if_ctrl_tm16xx {TM16XX_IFTYPE_3WSER, 5, 7, TM16XX_IC_SEGMUX_3 | TM16XX_IC_SEGBYTES_2, 0x3FFF, 5, 0x3F}  // SxG: 14x4-11x7 CC/CA, K:10x3, same as TM1623? TM16XX_IFTYPE_4WSPI - Datasheet v1.1 p2: "If DIN and DOUT use the same level, you can short-circuit DIN to multiplex DIN/DOUT"
 #define IC_TM1626A if_ctrl_tm16xx {TM16XX_IFTYPE_3WSER, 5, 7, TM16XX_IC_SEGMUX_3 | TM16XX_IC_SEGBYTES_2, 0x3BFF, 5, 0x09}  // SxG: 13x4-10x7 CC/CA, K:10x1, PWM controller for LED1-3, SW2/SW3 ports, see fig.28 on p12
 #define IC_TM1626B if_ctrl_tm16xx {TM16XX_IFTYPE_3WSER, 5, 7, TM16XX_IC_SEGMUX_3 | TM16XX_IC_SEGBYTES_2, 0x3FFF, 5, 0x3F}  // SxG: 14x4-11x7 CC/CA, K:10x3, PWM & SW2/SW3 (see TM1626A); chinese datasheet, 
 #define IC_TM1627  if_ctrl_tm16xx {TM16XX_IFTYPE_3WSER, 5, 7, TM16XX_IC_SEGMUX_3 | TM16XX_IC_SEGBYTES_2, 0x3BFF, 5, 0x1B}  // SxG: 13x4-10x7 CC/CA, K:10x2; KEYINT interrupt pin
 #define IC_TM1628A if_ctrl_tm16xx {TM16XX_IFTYPE_3WSER, 5, 7, TM16XX_IC_SEGMUX_3 | TM16XX_IC_SEGBYTES_2, 0x3BFF, 5, 0x1B}  // same as TM1628, but with internal pull-up/down resistors
-/*TODO*/ #define IC_TM1629  if_ctrl_tm16xx {TM16XX_IFTYPE_4WSPI, 5, 8, TM16XX_IC_SEGMUX_0 | TM16XX_IC_SEGBYTES_2, 0xFFFF, 4, 0xFF}  // SxG: 16x8 CC/CA, K:8x4
+#define IC_TM1629  if_ctrl_tm16xx {TM16XX_IFTYPE_3WSER, 5, 8, TM16XX_IC_SEGMUX_0 | TM16XX_IC_SEGBYTES_2, 0xFFFF, 4, 0xFF}  // SxG: 16x8 CC/CA, K:8x4; TM16XX_IFTYPE_4WSPI - datasheet v2.4 p2: "With DIN shorted for DIO to use"
 #define IC_TM1629A if_ctrl_tm16xx {TM16XX_IFTYPE_3WSER, 5, 8, TM16XX_IC_SEGMUX_0 | TM16XX_IC_SEGBYTES_2, 0xFFFF, 0, 0x00}  // SxG: 16x8 CC/CA
 #define IC_TM1629B if_ctrl_tm16xx {TM16XX_IFTYPE_3WSER, 5, 8, TM16XX_IC_SEGMUX_0 | TM16XX_IC_SEGBYTES_2, 0x3FFF, 4, 0xBB}  // SxG: 14x8 CC/CA, K:8x2
 #define IC_TM1629C if_ctrl_tm16xx {TM16XX_IFTYPE_3WSER, 5, 8, TM16XX_IC_SEGMUX_0 | TM16XX_IC_SEGBYTES_2, 0x7FFF, 4, 0x88}  // SxG: 15x8 CC/CA, K:8x1
